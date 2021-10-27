63453be02c19a29c84aac7b98cbe829af6392fa4
Prime2000-D <55726367+Prime2000-D@users.noreply.github.com>
2021-10-27 17:24:33 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,7 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBu1mtKvsd0UsgIUpALgVDCQZ0vf5d7prQ",
-  authDomain: "clone-49273.firebaseapp.com",
-  projectId: "clone-49273",
-  storageBucket: "clone-49273.appspot.com",
-  messagingSenderId: "517750284620",
-  appId: "1:517750284620:web:ee7ba57f9941afca1c2fe9",
-  measurementId: "G-J4BPE19GBW",
+  PUT_YOUR_FIREBASE_CONFIG
 };
 
 const app = initializeApp(firebaseConfig);
